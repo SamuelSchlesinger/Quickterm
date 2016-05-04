19fa4648959c602284bb8cc6cf178bce6cb1e8af
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-05-04 02:17:03 -0400

    Committing some things before I go on another experimental journey.

--- src/Quickterm.hs
+++ src/Quickterm.hs
@@ -8,7 +8,6 @@ module Quickterm
       , TerminalAction
       , quickrun
     )
-
 where
 
 import Text.EditDistance
@@ -20,18 +19,15 @@ type Name = String
 
 -- | With each of my endpoints for computation, I like to have some
 --   description of their usage. 
---
 type Usage = String
 
 -- | The sorts of actions which take place on the terminal are of
 --   the form "cmd {<arg> | ("--" | "-") <opt-name> <opt-val>}".
 --   The arguments are, in general, sequentially ordered.
---
 type Args = [String]
 
 -- | The options, on the other hand, tend to be a mapping from some
 --   set of names to some string.
---
 type Options = Map Name [String]
 
 -- | The terminal action itself should take some arguments, some options,
@@ -39,7 +35,6 @@ type Options = Map Name [String]
 --   The key here is that the TerminalAction is a pure function, taking
 --   pure values and deterministically attempting some IO action based on
 --   these. This is important to me in terms of designing and using programs. 
---
 type TerminalAction = Args -> Options -> IO ()
 
 -- | In general, the applications I use have two sorts of endpoints:
@@ -64,22 +59,19 @@ type TerminalAction = Args -> Options -> IO ()
 -- 
 --   A Command may simply have a Name, a TerminalAction, and a description
 --   of its Usage.
---  
 data Quickterm = Choice Name [Quickterm] Usage
                | Command Name TerminalAction Usage
 
--- | Gets the name from an arbitrary Quickterm
+--   Gets the name from an arbitrary Quickterm
 -- 
 qtName :: Quickterm -> String
 qtName (Choice name _ _) = name
 qtName (Command name _ _) = name
 
 -- | This function will generate the usage information and will be exposed at the top level.
---
-usage :: Quickterm -> String
 usage x = "usage: \n" ++ (usage' 0 x)
 
--- | This function will gather the usage information, indented properly for the given
+--   This function will gather the usage information, indented properly for the given
 --   depth.
 -- 
 usage' :: Int -> Quickterm  -> String
@@ -90,7 +82,6 @@ usage' n (Command name _ use) = (take n (repeat ' ')) ++ name ++ " " ++ use ++ "
 -- | This is a function which will take a top level program and attempt to run it,
 --   printing out the usage information for the deepest Choice achieved if the attempt 
 --   is failed, i.e we did not find a Command to run at all.
---   
 quickrun :: [String] -> Quickterm -> IO ()
 quickrun x q = quickrun' args opts q where
     (args, opts) = organizeInput x
@@ -102,23 +93,22 @@ quickrun x q = quickrun' args opts q where
                                                                             branches
                                                          Just branch -> quickrun' args opts branch
 
--- | Finds a branch, given a name, in a list of Quickterms
+--   Finds a branch, given a name, in a list of Quickterms
 -- 
 findbranch :: String -> [Quickterm] -> Maybe Quickterm
 findbranch _ [] = Nothing
 findbranch name ((choice@(Choice name' _ _)) : next) = if name == name' then Just choice else findbranch name next
 findbranch name ((command@(Command name' _ _)) : next) = if name == name' then Just command else findbranch name next
 
--- | Finds the nearest branch, if there is one, and prints out "Did you mean <branch>?",
+--   Finds the nearest branch, if there is one, and prints out "Did you mean <branch>?",
 --   followed by the usage, which should be passed as the second argument.
--- 
 whoopsy :: String -> String -> [Quickterm] -> IO ()
 whoopsy name use list = do
     case findnearestbranch name list of
         Nothing -> putStr use
         Just branch -> putStrLn ("Did you mean " ++ (qtName branch) ++ "?")
 
--- | Finds the branch which most nearly matches the name given,
+--   Finds the branch which most nearly matches the name given,
 --   starting the lowest edit distance at 100000 cause if you
 --   make your arguments long enough to get edit distances that high
 --   you're wrong.
@@ -132,7 +122,7 @@ findnearestbranch name list = findnearestbranch' name Nothing 3 list where
         else findnearestbranch' name nearest distance xs where
             distance' = levenshteinDistance defaultEditCosts name (qtName branch)
 
--- | Options are of the form {-}<opt-name> {<arg>}, where the number of arguments to
+--   Options are of the form {-}<opt-name> {<arg>}, where the number of arguments to
 --   the option is the number of dashes minus one.
 --
 --   The role of the rather nasty function below will be to extract the Options from
