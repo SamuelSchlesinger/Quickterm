32a2da0d3bda6e784b5f4d6be565f1661b61044a
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2016-05-05 21:35:25 +0200

    Rewritten Quickterms to form a core language.
    Due to these changes, the Quickterm data type represents now a programming
    language. The predicates form computable actions which are applied on an input
    program. They are used to decide which path of the Quickterm-program will be
    taken. The resulting TermAction forms the actual executing code.
    
    By that Quickterm is a command line argument parser interpreter. Whew, what
    a name...
    
    The code for Quickterm predicates can be formulated with simple Haskell
    lambdas and form by that an EDSL. The Help-type is used for generation of help
    messages. Due to function composition it is possible to form an EDSL for
    manipulation of help messages. This makes it possible to adjust indentation
    like seen in 'defaultWs' and do various other things. I could think of an EDSL
    for composition of TermActions as the use of 'const' gives slight inside about
    parameter skipping. A conversion EDSL could be used for easy serialization of
    data.
    
    At runtime the interpreter (implemented in 'runQuickterm') all predicates will
    be applied on the argument list. The result of this application is used to
    calculate a penalty. E.g. you can filter for (penalty==0) to find full matched
    TermActions. Multiple results mean ambiguous predicates.
    
    Due to penalty returning, it is now possible to calculate the most presumable
    command line for error correction for further help for the user of the command
    line application.

--- src/Quickterm.hs
+++ src/Quickterm.hs
@@ -1,140 +1,66 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses #-}
+
 module Quickterm
-    ( Quickterm(..)
-    , Options
-    , Name
-    , Args
-    , usage
-    , TerminalAction
-    , quickrun
+    (
     ) where
 
+import Data.Foldable (asum)
 import Text.EditDistance
-import qualified Data.HashMap as M
+import Control.Applicative
+import Control.Monad
+import Control.Arrow (first)
+import Data.List (intercalate)
 
--- | I tend to call my applications by some name
---
-type Name = String
+type Predicate = String -> Int
+type TermAction = [String] -> IO ()
+type Help = Int -> String
 
--- | With each of my endpoints for computation, I like to have some
---   description of their usage.
-type Usage = String
+data Quickterm
+  = Choice [(Predicate, Quickterm)] Help
+  | Action TermAction Help
 
--- | The sorts of actions which take place on the terminal are of
---   the form "cmd {<arg> | ("--" | "-") <opt-name> <opt-val>}".
---   The arguments are, in general, sequentially ordered.
-type Args = [String]
+runQuickterm :: Quickterm -> [String] -> [(Int, [String] -> IO ())]
+runQuickterm (Action t  _) []     = pure (0, t)
+runQuickterm c@Choice{} []        = pure (0, const . putStrLn $ makeHelp 0 1 c)
+runQuickterm (Action t  _) _      = pure (0, t)
+runQuickterm (Choice ps _) (a:as) =
+  ps >>= \(p,q) ->
+  runQuickterm q as >>= \(i, io) ->
+  return (p a + i, io)
 
--- | The options, on the other hand, tend to be a mapping from some
---   set of names to some string.
-type Options = M.Map Name [String]
+makeHelp :: Int -> Int -> Quickterm -> String
+makeHelp l i (Choice ps u) =
+  if   i >= 0
+  then u l ++ "\n" ++ intercalate (ws i ++ "\n") (makeHelp (l+1) (i - 1) . snd <$> ps)
+  else u l
+makeHelp l _ (Action _ u) = u l
 
--- | The terminal action itself should take some arguments, some options,
---   and be able to produce some IO action dependent based on these.
---   The key here is that the TerminalAction is a pure function, taking
---   pure values and deterministically attempting some IO action based on
---   these. This is important to me in terms of designing and using programs.
-type TerminalAction = Args -> Options -> IO ()
+ws :: Int -> String
+ws l = replicate (l*2) ' '
 
--- | In general, the applications I use have two sorts of endpoints:
---
---   1) The application has a main command which takes arguments and options
---      and performs basically the same behavior, with some variation, based
---      on these inputs.
---
---   2) The application has a variety of sub-modules which have their own pretty
---      unique functionality and usage. I prefer these for two reasons: The first
---      is that, as the user, it separates the learning curve for one aspect of
---      the program from some other one. The second is that it allows me as the
---      developer of such software to truly make modular systems and plug them in
---      and unplug them as I wish.
---
---   As I like this and I am developing this, I will then focus my efforts as such.
---   We shall call a program which contain submodules a Choice, whereas we shall
---   call a program which is simply a command to be run a Command.
---
---   A Choice should simply have a Name, a list of Quickterms, and
---   a description of its Usage.
---
---   A Command may simply have a Name, a TerminalAction, and a description
---   of its Usage.
-data Quickterm = Choice Name [Quickterm] Usage
-               | Command Name TerminalAction Usage
+defaultWs :: String -> Int -> String
+defaultWs a i = intercalate "\n" ((ws i ++) <$> splitLn a)
 
---   Gets the name from an arbitrary Quickterm
---
-qtName :: Quickterm -> String
-qtName (Choice name _ _) = name
-qtName (Command name _ _) = name
+splitLn :: String -> [String]
+splitLn = f []
+  where
+    f rs []        = [reverse rs]
+    f rs ('\n':ss) = reverse rs : f [] ss
+    f rs (s:ss)    = f (s:rs) ss
 
--- | This function will generate the usage information and will be exposed at the top level.
-usage x = "usage: \n" ++ usage' 0 x
+execQuickterm :: Quickterm -> [String] -> IO ()
+execQuickterm q as = void . sequence $ (\q -> q as) <$> valid
+  where
+    rs = runQuickterm q as
+    valid = snd <$> filter (\(i,_) -> i == 0) rs
 
---   This function will gather the usage information, indented properly for the given
---   depth.
---
-usage' :: Int -> Quickterm  -> String
-usage' n (Choice name branches use) = replicate n ' ' ++ name ++ " " ++ use ++ "\n"
-                                    ++ concatMap (usage' (n + 4)) branches
-usage' n (Command name _ use) = replicate n ' ' ++ name ++ " " ++ use ++ "\n"
+l = levenshteinDistance defaultEditCosts
 
--- | This is a function which will take a top level program and attempt to run it,
---   printing out the usage information for the deepest Choice achieved if the attempt
---   is failed, i.e we did not find a Command to run at all.
-quickrun :: [String] -> Quickterm -> IO ()
-quickrun x = quickrun' args opts where
-    (args, opts) = organizeInput x
-    quickrun' [] _ (Choice _ _ use) = putStr use -- No way to make a choice without input
-    quickrun' args opts (Command _ action _) = action args opts
-    quickrun' (nextbranch:args) opts choice@(Choice name branches use) = case findbranch nextbranch branches of
-                                                         Nothing -> whoopsy nextbranch
-                                                                           (usage choice)
-                                                                            branches
-                                                         Just branch -> quickrun' args opts branch
-
---   Finds a branch, given a name, in a list of Quickterms
---
-findbranch :: String -> [Quickterm] -> Maybe Quickterm
-findbranch _ [] = Nothing
-findbranch name ((choice@(Choice name' _ _)) : next) = if name == name' then Just choice else findbranch name next
-findbranch name ((command@(Command name' _ _)) : next) = if name == name' then Just command else findbranch name next
-
---   Finds the nearest branch, if there is one, and prints out "Did you mean <branch>?",
---   followed by the usage, which should be passed as the second argument.
-whoopsy :: String -> String -> [Quickterm] -> IO ()
-whoopsy name use list = case findnearestbranch name list of
-  Just branch -> putStrLn ("Did you mean " ++ qtName branch ++ "?")
-  _           -> putStr use
-
---   Finds the branch which most nearly matches the name given,
---   starting the lowest edit distance at 100000 cause if you
---   make your arguments long enough to get edit distances that high
---   you're wrong.
---
-findnearestbranch :: String -> [Quickterm] -> Maybe Quickterm
-findnearestbranch name = findnearestbranch' name Nothing 3 where
-    findnearestbranch' :: String -> Maybe Quickterm -> Int -> [Quickterm] -> Maybe Quickterm
-    findnearestbranch' _ mqt _ [] = mqt
-    findnearestbranch' name nearest distance (branch:xs) = if distance > distance'
-        then findnearestbranch' name (Just branch) distance' xs
-        else findnearestbranch' name nearest distance xs where
-            distance' = levenshteinDistance defaultEditCosts name (qtName branch)
-
---   Options are of the form {-}<opt-name> {<arg>}, where the number of arguments to
---   the option is the number of dashes minus one.
---
---   The role of the rather nasty function below will be to extract the Options from
---   the list of strings and return the list of strings.
---
-organizeInput :: [String] -> (Args, Options)
-organizeInput x = (args, opts) where
-    (args, opts) = organizeInput' x [] M.empty
-    organizeInput' :: [String] -> Args -> Options -> (Args, Options)
-    organizeInput' [] args opts = (reverse args, opts)
-    organizeInput' (('-':restopt):xs) args opts = organizeInput' rest args
-                                                                (M.insert name vals opts) where
-        (name, rest, vals) = getOpt restopt xs [] 0
-        getOpt :: String -> [String] -> [String] -> Int -> (String, [String], [String])
-        getOpt ('-':more) xs args n = getOpt more xs args (n + 1)
-        getOpt name xs args n = (name, rest, vals) where
-            (vals, rest) = splitAt n xs
-    organizeInput' (x:xs) args opts = organizeInput' xs (x:args) opts
+example :: Quickterm
+example = Choice
+  [ (l "install", Action (const $ putStrLn "installation process") . defaultWs $ "install -- install a package")
+  , (l "sandbox", Choice
+    [ (l "init", Action (const $ putStrLn "initializing a sandbox") . defaultWs $ "init\n  -- initialize a sandbox")
+    , (l "clear", Action (const $ putStrLn "clearing the current sandbox") . defaultWs $ "clear\n  -- clear the current sandbox")
+    ] . defaultWs $ "sandbox -- Sandbox related actions.")
+  ] . defaultWs $ "Description of your application"
