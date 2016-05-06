80ed1399e335f5208117defe95d5484c705ac601
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2016-05-06 15:04:32 +0200

    Implemented sub language for marshaling.

--- src/Quickterm.hs
+++ src/Quickterm.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses #-}
-
 module Quickterm
     (
     ) where
@@ -11,8 +9,8 @@ import Control.Monad
 import Control.Arrow (first)
 import Data.List (intercalate)
 import Data.Char
-import Text.Regex.Base
-import Text.Regex.TDFA
+import Text.Regex.Base hiding (empty)
+import Text.Regex.TDFA hiding (empty)
 
 type Predicate = String -> Int
 type TermAction = [String] -> IO ()
@@ -72,6 +70,50 @@ section n ds = (exact n, Choice ds (indent $ "== " ++ n ++ " =="))
 
 command :: String -> String -> TermAction -> (Predicate,Quickterm)
 command n d t = (exact n, Action t (indent $ n ++ "\n-- " ++ d))
+
+newtype Marshaler f a = Marshaler { marshal :: [String] -> f (a, [String]) }
+
+instance (Functor f) => Functor (Marshaler f) where
+  fmap f m = Marshaler $ \as -> first f `fmap` (marshal m as)
+
+instance (Monad f) => Applicative (Marshaler f) where
+  pure a = Marshaler $ \as -> pure (a,as)
+  f <*> m = Marshaler $ \as -> marshal f as >>= \(g, as') -> marshal m as' >>= \(a, as'') -> return (g a, as'')
+
+instance (MonadPlus f) => Alternative (Marshaler f) where
+  empty = Marshaler $ const empty
+  m <|> n = Marshaler $ \as -> marshal m as <|> marshal n as
+
+instance (Monad f) => Monad (Marshaler f) where
+  return = pure
+  m >>= f = Marshaler $ \as -> marshal m as >>= \(a, as') -> marshal (f a) as'
+
+instance (MonadPlus f) => MonadPlus (Marshaler f) where
+  mzero = empty
+  mplus = (<|>)
+
+aInt :: (Alternative f) => Marshaler f Int
+aInt = Marshaler $ \as -> case as of
+  []     -> empty
+  (a:as) -> if   a =~ "((0|1|2|3|4|5|6|7|8|9)+)"
+            then pure (read $ a, as)
+            else empty
+
+aString :: (Alternative f) => Marshaler f String
+aString = Marshaler $ \as -> case as of
+  []     -> empty
+  (a:as) -> if   a =~ "([^-]+)"
+            then pure (a, as)
+            else empty
+
+exampleMarshaling :: (MonadPlus f) => f (Int, [String])
+exampleMarshaling = marshal ((\x y z -> x + y + z) <$> (aInt >>= return . (* 2)) <*> aInt <*> aInt) ["10", "20", "30"]
+
+genericPenalty :: (Foldable f) => f a -> Int
+genericPenalty = regexPenalty . not . null
+
+m2pred :: (MonadPlus f, Foldable f) => Marshaler f a -> Predicate
+m2pred m s = genericPenalty (marshal m [s])
 
 program :: String -> [(Predicate,Quickterm)] -> Quickterm
 program d ds = Choice ds (indent d)
