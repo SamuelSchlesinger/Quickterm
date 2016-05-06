4174a6f10038a4f796d1e7c126a881e43486ce75
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2016-05-06 17:23:57 +0200

    Removed old implementation.

--- src/Quickterm.hs
+++ src/Quickterm.hs
@@ -16,26 +16,6 @@ type Predicate = String -> Int
 type TermAction = [String] -> IO ()
 type Help = Int -> String
 
-data Quickterm
-  = Choice [(Predicate, Quickterm)] Help
-  | Action TermAction Help
-
-runQuickterm :: Quickterm -> [String] -> [(Int, [String] -> IO ())]
-runQuickterm (Action t  _) []     = pure (0, t)
-runQuickterm c@Choice{} []        = pure (0, const . putStrLn $ makeHelp 0 1 c)
-runQuickterm (Action t  _) _      = pure (0, t)
-runQuickterm (Choice ps _) (a:as) =
-  ps >>= \(p,q) ->
-  runQuickterm q as >>= \(i, io) ->
-  return (p a + i, io)
-
-makeHelp :: Int -> Int -> Quickterm -> String
-makeHelp l i (Choice ps u) =
-  if   i >= 0
-  then u l ++ "\n" ++ intercalate (ws i ++ "\n") (makeHelp (l+1) (i - 1) . snd <$> ps)
-  else u l
-makeHelp l _ (Action _ u) = u l
-
 ws :: Int -> String
 ws l = replicate (l*2) ' '
 
@@ -48,12 +28,6 @@ splitLn = f []
     f rs []        = [reverse rs]
     f rs ('\n':ss) = reverse rs : f [] ss
     f rs (s:ss)    = f (s:rs) ss
-
-execQuickterm :: Quickterm -> [String] -> IO ()
-execQuickterm q as = void . sequence $ (\q -> q as) <$> valid
-  where
-    rs = runQuickterm q as
-    valid = snd <$> filter (\(i,_) -> i == 0) rs
 
 regexPenalty :: Bool -> Int
 regexPenalty True = 0
@@ -110,20 +84,20 @@ predicate n u f = Marshaler $ \i as h pi -> case as of
 exampleMarshaling :: [(Int, Int, [String], Help, [String])]
 exampleMarshaling = marshal (aInt >>= \x -> aInt >>= \y -> aInt >>= \z -> return $ x + y + z) 0 ["10", "20", "30"] (const "foo") []
 
-program :: String -> [(Predicate,Quickterm)] -> Quickterm
-program d ds = Choice ds (indent d)
+--program :: String -> [(Predicate,Quickterm)] -> Quickterm
+--program d ds = Choice ds (indent d)
 
-section :: String -> [(Predicate,Quickterm)] -> (Predicate,Quickterm)
-section n ds = (exact n, Choice ds (indent $ "== " ++ n ++ " =="))
+--section :: String -> [(Predicate,Quickterm)] -> (Predicate,Quickterm)
+--section n ds = (exact n, Choice ds (indent $ "== " ++ n ++ " =="))
 
-command :: String -> String -> TermAction -> (Predicate,Quickterm)
-command n d t = (exact n, Action t (indent $ n ++ "\n-- " ++ d))
+--command :: String -> String -> TermAction -> (Predicate,Quickterm)
+--command n d t = (exact n, Action t (indent $ n ++ "\n-- " ++ d))
 
-example :: Quickterm
-example = program "Description of your application"
-  [ command "install" "installs a package" . const $ putStrLn "installation process"
-  , section "sandbox"
-    [ command "init" "initialize a sandbox" . const $ putStrLn "initializing a sandbox"
-    , command "clear" "clear the current sandbox" . const $ putStrLn "clearing the current sandbox"
-    ]
-  ]
+--example :: Quickterm
+--example = program "Description of your application"
+--  [ command "install" "installs a package" . const $ putStrLn "installation process"
+--  , section "sandbox"
+--    [ command "init" "initialize a sandbox" . const $ putStrLn "initializing a sandbox"
+--    , command "clear" "clear the current sandbox" . const $ putStrLn "clearing the current sandbox"
+--    ]
+--  ]
