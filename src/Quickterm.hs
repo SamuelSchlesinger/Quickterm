a328f87d720a5f3b740863dec7e8de398875cc2f
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2016-05-06 23:57:34 +0200

    Implemented marshaling.
    Marshaling of simple and complex data types is now possible. Injection of the 'param' method can be done by implementing the typeclass 'CanMarshal' for a specific data type.
    
    TODO:
    
    * implement automatic help message generation
    * implement hooks for help message generation in a separate EDSL
    * implement filtering in 'quickterm' to find ambiguous calls and full error inputs
    * implement printing of help messages and call suggestions
    * implement syntax tree for ambiguous call analysis to report errors at compile time
    * implement DSL for full wrapping of library interface

--- src/Quickterm.hs
+++ src/Quickterm.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+
 module Quickterm
     (
     ) where
@@ -39,50 +41,120 @@ regex r = regexPenalty . (=~ r)
 exact :: String -> String -> Int
 exact = levenshteinDistance defaultEditCosts
 
-newtype Quickterm a = Quickterm { runQuickterm :: Int -> [String] -> Help -> [String] -> [(Int, a, [String], Help, [String])] }
+newtype Quickterm a = Quickterm { runQuickterm :: Int -> Help -> [String] -> [String] -> [(a, Int, Help, [String], [String])] }
 
 instance Functor Quickterm where
-  fmap f m = Quickterm $ \i as h pi -> (\(i',a,as',h',pi') -> (i',f a,as',h',pi')) `fmap` (runQuickterm m i as h pi)
+  fmap f m = Quickterm $ \i h pi as -> (\(a,i',h',pi',as') -> (f a,i',h',pi',as')) `fmap` (runQuickterm m i h pi as)
 
 instance Applicative Quickterm where
-  pure a = Quickterm $ \i as h pi -> pure (i,a,as,h,pi)
-  f <*> m = Quickterm $ \i as h pi -> runQuickterm f i as h pi >>=
-                        \(i' ,g, as' ,h' , pi') -> runQuickterm m i' as' h' pi' >>=
-                        \(i'',a, as'',h'', pi'') -> return (i'',g a,as'',h'',pi'')
+  pure a = Quickterm $ \i h pi as -> pure (a,i,h,pi,as)
+  f <*> m = Quickterm $ \i h pi as -> runQuickterm f i h pi as
+                      >>= \(g,i' ,h' , pi', as' ) -> runQuickterm m i' h' pi' as'
+                      >>= \(a,i'',h'', pi'',as'') -> return (g a,i'',h'',pi'',as'')
 
 instance Alternative Quickterm where
   empty = Quickterm (const (const (const (const empty))))
-  m <|> n = Quickterm $ \i as h pi -> filter (\(c,_,_,_,_) -> c >= 1000) $ runQuickterm m i as h pi <|> runQuickterm n i as h pi
+  m <|> n = Quickterm $ \i h pi as -> filter (\(_,c,_,_,_) -> c >= 1000) $ runQuickterm m i h pi as <|> runQuickterm n i h pi as
 
 instance Monad Quickterm where
   return = pure
-  m >>= f = Quickterm $ \i as h pi -> runQuickterm m i as h pi >>= \(i',a,as',h',pi') -> runQuickterm (f a) i' as' h' pi'
+  m >>= f = Quickterm $ \i h pi as -> runQuickterm m i h pi as >>= \(a,i',h',pi',as') -> runQuickterm (f a) i' h' pi' as'
 
 instance MonadPlus Quickterm where
   mzero = empty
   mplus = (<|>)
 
-aInt :: Quickterm Int
-aInt = Quickterm $ \i as h pi -> case as of
-  []      -> pure (i+10,0,[],h,pi)
-  (a:as') -> if   a =~ "((0|1|2|3|4|5|6|7|8|9)+)"
-             then [(i,read a, as',h,a:pi)]
-             else [(i+10,0,as',h,show 0:pi), (i+10,0,(a:as'),h,show 0:pi)]
 
-aString :: Quickterm String
-aString = Quickterm $ \i as h pi -> case as of
-  []      -> pure (i+10,empty,[],h,pi)
-  (a:as') -> if   a =~ "([^-]+)"
-             then pure (i,a,as',h,a:pi)
-             else pure (i+10,empty,as',h,"str":pi) <|> pure (i+10,empty,(a:as'),h,"str":pi)
+newtype Deserializer a = Deserializer { deserialize :: [Char] -> Int -> [(a,[Char],Int)] }
 
-predicate :: String -> Help -> Predicate -> Quickterm ()
-predicate n u f = Quickterm $ \i as h pi -> case as of
-  []      -> pure (i+10,(),[],h,n:pi)
-  (a:as') -> pure (i+f a,(),as',h,n:pi)
+instance Functor Deserializer where
+  fmap f d = Deserializer $ \st p -> fmap (\(a,st',p') -> (f a,st',p')) (deserialize d st p)
 
-exampleMarshaling :: [(Int, Int, [String], Help, [String])]
-exampleMarshaling = runQuickterm (aInt >>= \x -> aInt >>= \y -> aInt >>= \z -> return $ x + y + z) 0 ["10", "20", "30"] (const "foo") []
+instance Applicative Deserializer where
+  pure a = Deserializer $ \st p -> [(a,st,p)]
+  f <*> d = Deserializer $ \st p -> deserialize f st p
+                         >>= \(g,st',p') -> deserialize d st' p'
+                         >>= \(a,st'',p'') -> return (g a,st'',p'')
+
+instance Alternative Deserializer where
+  empty = Deserializer (const (const empty))
+  d <|> h = Deserializer $ \st p -> filter (\(_,_,i) -> i >= 1000) $ deserialize d st p <|> deserialize h st p
+
+instance Monad Deserializer where
+  return = pure
+  d >>= f = Deserializer $ \st p -> deserialize d st p >>= \(d',st',p') -> deserialize (f d') st' p'
+
+instance MonadPlus Deserializer where
+  mzero = empty
+  mplus = (<|>)
+
+tryConvert :: (String -> [(a,Int)]) -> Deserializer a
+tryConvert f = Deserializer $ \st p -> (\(a,i) -> (a,[],p+i)) <$> f st
+
+
+data Description = Description
+  { nameD :: String
+  , longD :: Int -> String
+  }
+
+class CanMarshal a where
+  helpU :: a -> Int -> String
+  deserializer :: Deserializer a
+
+instance CanMarshal Int where
+  helpU _ = indent "<Integer>"
+  deserializer = tryConvert $ \st ->
+    if   st =~ "((0|1|2|3|4|5|6|7|8|9)+)"
+    then [(read $ st,0)]
+    else [(0,length st * 2)]
+
+instance CanMarshal String where
+  helpU _ = indent "<String>"
+  deserializer = tryConvert $ \st ->
+    if   st =~ "([^-]+)"
+    then [(st,0)]
+    else [("str",length st * 2)]
+
+bar :: String -> IO ()
+bar m = print $ deserialize deserializer m 0 >>= \(a, i, _) -> return (a::String,i)
+
+param :: (Show a, CanMarshal a) => Quickterm a
+param = Quickterm $ \i h pi as -> case as of
+  []      -> empty -- TODO: implement punishment
+  (a:as') -> deserialize deserializer a 0 >>= \(a, _, i') -> return (a, i + i', h, show a:pi, as')
+
+--instance CanMarshal Symbol where
+--  defaultM = Symbol ""
+--  helpU a = indent "a"
+--  deserializer = tryConvert $ \st -> [(s,exact (getName s) st)]
+
+desc :: String -> Description
+desc n = Description n (const "")
+
+section :: Description -> [Quickterm a] -> Quickterm a
+section (Description n h) qs = Quickterm $ \i h pi as -> case as of
+  []      -> empty
+  (a:as') -> qs >>= \m -> runQuickterm m (i + exact n a) h (n:pi) as'
+
+program :: [Quickterm a] -> Quickterm a
+program qs = Quickterm $ \i h pi as -> qs >>= \m -> runQuickterm m i h pi as
+
+foo = section ((desc "foo")
+        { longD = const "my long description of section foo" })
+    [ section (desc "install") [ cmdInstall <$> param <*> param ]
+    ]
+
+results qt as = quickterm qt as >>= \(a,i,_,pi,_) -> return (a,i,reverse pi)
+
+quickterm :: Quickterm a -> [String] -> [(a, Int, Help, [String], [String])]
+quickterm qt = runQuickterm qt 0 (const "foo") []
+
+cmdInstall :: String -> String -> IO ()
+cmdInstall app v = do
+  putStrLn $ "installing " ++ app
+  putStrLn $ "with version " ++ v
+  putStrLn "done"
+  putStrLn ""
 
 --program :: String -> [(Predicate,Quickterm)] -> Quickterm
 --program d ds = Choice ds (indent d)
