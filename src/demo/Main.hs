7e034b20cb6b41e3d2df7c4caad5433d63baacc4
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2016-05-09 13:19:26 +0200

    Further refactoring of package layout.

---
+++ src/demo/Main.hs
@@ -0,0 +1,5 @@
+module Main where
+
+main = do
+  putStrLn "hi"
+  putStrLn "bye"
---
+++ src/lib/System/Console/Quickterm/DSL.hs
@@ -0,0 +1,8 @@
+module System.Console.Quickterm.DSL
+    (
+    ) where
+
+import Text.ParserCombinators.UU
+import Text.ParserCombinators.UU
+
+
